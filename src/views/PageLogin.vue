452e6c9114beca80e55d2c5283cc41c67bf1e874
Louis Charette <charette.louis@gmail.com>
2024-07-25 20:58:37 -0400

    Auth Guard code moved to sprinkle-account

--- src/views/PageLogin.vue
+++ src/views/PageLogin.vue
@@ -1,27 +1,5 @@
 <script setup lang="ts">
-import { watch } from 'vue'
-import { useRouter } from 'vue-router'
-import { useAuthStore } from '@userfrosting/sprinkle-account/stores'
 import FormLogin from '../components/Content/FormLogin.vue'
-
-// Stores
-const auth = useAuthStore()
-const router = useRouter()
-
-// Watch for redirect
-// TODO : Replace route guard with initial state check
-// TODO : Should be moved to a composable or auth store, as this will be useful in other places,
-// TODO : Should also use query redirect (to redirect where we were before login)
-// @see : https://vue-auth3.js.org/guide/auth-meta.html#advanced-redirects
-watch(
-    () => auth.isAuthenticated,
-    (isAuthenticated) => {
-        if (isAuthenticated === true) {
-            let redirectTo = router.currentRoute.value.meta.redirectAfterLogin ?? '/'
-            router.push(redirectTo)
-        }
-    }
-)
 </script>
 
 <template>
