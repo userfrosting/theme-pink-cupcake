929bac73ee5d715aa5f079f7570096aaf1ee956b
Louis Charette <charette.louis@gmail.com>
2024-10-04 20:53:00 -0400

    Add Sprunje Components

---
+++ src/components/Sprunjer/SprunjeTable.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useSprunjer } from '@userfrosting/sprinkle-core/sprunjer'
+import SprunjePaginator from './SprunjePaginator.vue'
+
+const props = defineProps({
+    dataUrl: {
+        type: String,
+        required: true
+    },
+    hidePagination: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const sprunjer = useSprunjer(props.dataUrl)
+const { rows, fetch, loading } = sprunjer
+</script>
+
+<template>
+    <slot name="actions" :sprunjer="{ fetch, loading }"></slot>
+    <table class="uk-table uk-table-striped">
+        <thead>
+            <tr>
+                <slot name="header"></slot>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in rows" :key="row.id">
+                <slot name="body" :item="row"></slot>
+            </tr>
+        </tbody>
+    </table>
+    <slot v-if="!hidePagination" name="paginator">
+        <SprunjePaginator :sprunjer="sprunjer" />
+    </slot>
+</template>
