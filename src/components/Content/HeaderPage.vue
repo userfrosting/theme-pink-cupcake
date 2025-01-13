9dc0d7ec200ac81ebfff476dfa39ef09131fc38b
Louis Charette <charette.louis@gmail.com>
2025-01-12 21:02:30 -0500

    Use Grid instead of align-right

--- src/components/Content/HeaderPage.vue
+++ src/components/Content/HeaderPage.vue
@@ -1,34 +1,51 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { usePageMeta } from '@userfrosting/sprinkle-core/composables'
 const page = usePageMeta()
+
+const shouldShowHeader = computed(() => {
+    return page.title || page.description || page.breadcrumbs.length > 1
+})
+
+const shouldShowTitle = computed(() => {
+    return page.title && page.hideTitle === false
+})
+
+const shouldShowDescription = computed(() => {
+    return page.description && page.hideTitle === false
+})
+
+const shouldShowBreadcrumbs = computed(() => {
+    return page.breadcrumbs.length > 1 && page.hideBreadcrumbs === false
+})
 </script>
 
 <template>
-    <div
-        class="uf-page-header"
-        v-if="page.title || page.description || page.breadcrumbs.length > 1">
-        <nav
-            aria-label="Breadcrumb"
-            v-if="page.breadcrumbs.length > 1 && page.hideBreadcrumbs === false">
-            <ul class="uk-breadcrumb uk-align-right">
-                <li v-for="(crumb, index) in page.breadcrumbs" :key="index">
-                    <span aria-current="page" v-if="index + 1 === page.breadcrumbs.length">
-                        {{ page.title }}
-                    </span>
-                    <router-link v-else :to="crumb.to">
-                        <font-awesome-icon icon="house" fixed-width v-if="index === 0" />
-                        {{ crumb.label }}
-                    </router-link>
-                </li>
-            </ul>
-        </nav>
-        <h3 v-if="page.title && page.hideTitle === false" class="uk-margin-remove">
-            {{ page.title }}
-        </h3>
-        <p
-            v-if="page.description && page.hideTitle === false"
-            class="uk-text-meta uk-margin-remove">
-            {{ page.description }}
-        </p>
+    <div class="uf-page-header" v-if="shouldShowHeader">
+        <div uk-grid>
+            <div class="uk-width-expand">
+                <h3 v-if="shouldShowTitle" class="uk-margin-remove">
+                    {{ page.title }}
+                </h3>
+                <p v-if="shouldShowDescription" class="uk-text-meta uk-margin-remove">
+                    {{ page.description }}
+                </p>
+            </div>
+            <div>
+                <nav aria-label="Breadcrumb" v-if="shouldShowBreadcrumbs">
+                    <ul class="uk-breadcrumb">
+                        <li v-for="(crumb, index) in page.breadcrumbs" :key="index">
+                            <span aria-current="page" v-if="index + 1 === page.breadcrumbs.length">
+                                {{ page.title }}
+                            </span>
+                            <router-link v-else :to="crumb.to">
+                                <font-awesome-icon icon="house" fixed-width v-if="index === 0" />
+                                {{ crumb.label }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </div>
 </template>
