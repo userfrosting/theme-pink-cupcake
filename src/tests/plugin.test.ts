16da2531e0b7b98265be7ae2e91e8886c36b45e5
Louis Charette <charette.louis@gmail.com>
2025-09-06 18:33:53 -0400

    Fixing warning during test

--- src/tests/plugin.test.ts
+++ src/tests/plugin.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test, vi } from 'vitest'
-import { createApp } from 'vue'
+import { createApp, ref } from 'vue'
 import plugins from '..'
 import Components from '../plugins/components'
 import * as AlertsStore from '@userfrosting/sprinkle-core/stores'
@@ -9,7 +9,12 @@ describe('plugins.ts', () => {
         const app = createApp({})
 
         vi.spyOn(Components, 'install')
-        vi.spyOn(AlertsStore, 'useAlertsStore').mockReturnValue(vi.fn() as any)
+        vi.spyOn(AlertsStore, 'useAlertsStore').mockReturnValue({
+            alerts: ref([]),
+            addAlert: vi.fn(),
+            removeAlert: vi.fn()
+            // add any other properties/methods expected by the plugin
+        } as any)
 
         plugins.install(app)
         expect(Components.install).toHaveBeenCalled()
