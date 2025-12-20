6a1e8cf2ff3db927d168c1cf1a79112e165eb945
Louis Charette <charette.louis@gmail.com>
2025-12-19 21:39:18 -0500

    Fix style

--- src/components/Content/MainContent.vue
+++ src/components/Content/MainContent.vue
@@ -1,7 +1,5 @@
 <template>
-    <main
-        class="uk-section uk-section-default uk-section-muted"
-        uk-height-viewport="expand: true">
+    <main class="uk-section uk-section-default uk-section-muted" uk-height-viewport="expand: true">
         <div class="uk-container" data-test="slot">
             <slot></slot>
         </div>
